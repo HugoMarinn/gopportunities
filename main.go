b08d64ef337bdec05d24a763b2e2ed44d45d7b5f
HugoMarinn <hugomc13952@gmail.com>
2025-01-24 21:03:13 -0300

    feat: add opening schema and custom logger

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"github.com/HugoMarinn/gopportunities/config"
 	"github.com/HugoMarinn/gopportunities/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config inicialization error: %v", err)
+		return 
+	}
+
 	router.Initialize()
 }
